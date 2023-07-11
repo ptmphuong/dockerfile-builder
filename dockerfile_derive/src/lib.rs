516126ed71102f86cdbb5e99eb2e24f3d0465817
Phuong Pham <ptmphuong9@gmail.com>
2023-07-11 15:59:53 -0700

    support generics for Vec<String> fields + refactor

--- dockerfile_derive/src/lib.rs
+++ dockerfile_derive/src/lib.rs
@@ -1,5 +1,6 @@
+mod utils;
+
 use proc_macro::TokenStream;
-use proc_macro2::TokenTree;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
@@ -10,7 +11,7 @@ pub fn instruction_init(input: TokenStream) -> TokenStream {
 
     let variants = match input.data {
         syn::Data::Enum(syn::DataEnum { ref variants, .. }, .. ) => variants,
-        _ => return make_err(&input.ident, "Expected Enum").into(),
+        _ => return utils::make_err(&input.ident, "Expected Enum").into(),
     };
 
     let impl_display = variants.iter().map(|v| {
@@ -106,7 +107,7 @@ pub fn instruction_builder(input: TokenStream) -> TokenStream {
     );
 
     let attr = &input.attrs;
-    let (instruction_name, value_method) = match get_attr(attr, struct_ident) {
+    let (instruction_name, value_method) = match utils::get_attr(attr, struct_ident) {
         Ok(ad) => (ad.instruction_name, ad.value_method),
         Err(e) => return e.into(),
     };
@@ -116,7 +117,7 @@ pub fn instruction_builder(input: TokenStream) -> TokenStream {
             fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
             ..
         }) => named,
-        _ => return make_err(struct_ident, "Expected Struct with named fields").into(),
+        _ => return utils::make_err(struct_ident, "Expected Struct with named fields").into(),
     };
 
     let builder_empty = fields.iter().map(|f| {
@@ -128,7 +129,7 @@ pub fn instruction_builder(input: TokenStream) -> TokenStream {
     let builder_field = fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-        if inner_type("Option", ty).is_some() {
+        if utils::is_type("Option", ty) {
             quote! { #name: #ty, }
         } else {
             quote! { #name: std::option::Option<#ty>, }
@@ -139,87 +140,113 @@ pub fn instruction_builder(input: TokenStream) -> TokenStream {
         let name = &f.ident;
 
         let original_ty = &f.ty;
-        let inner_ty = inner_type("Option", original_ty);
-        let set_ty = inner_ty.unwrap_or(original_ty);
 
-        if is_type("String", set_ty) {
-            quote! { 
+        // Custom set method for Vec<String> or Option<Vec<String>
+        // These methods can accept Vec<T> where T: Into<String> as argument.
+        if utils::is_type_vec_string(&original_ty) ||
+            utils::is_type_option_vec_string(&original_ty) {
+            return quote! {
+                pub fn #name<T: Into<String>>(&mut self, #name: Vec<T>) -> &mut Self {
+                    let converted = #name.into_iter().map(|s| s.into()).collect::<Vec<String>>();
+                    self.#name = Some(converted);
+                    self
+                }
+            };
+        }
+
+        // Custom set method for String or Option<String>
+        // These methods can accept Vec<T> where T: Into<String> as argument.
+        if utils::is_type_option_string(&original_ty) ||
+            utils::is_type("String", &original_ty) {
+            return quote! { 
                 pub fn #name<T: Into<String>>(&mut self, #name: T) -> &mut Self {
                     self.#name = Some(#name.into());
                     self
                 }
-            }
+            };
+        }
 
-        } else {
-            quote! { 
-                pub fn #name(&mut self, #name: #set_ty) -> &mut Self {
-                    self.#name = Some(#name);
-                    self
-                }
+        // Defaut set method.
+        // If original type is Option<inner> => set type is inner
+        // Else set type is original type
+        let set_ty = utils::inner_type("Option", original_ty)
+            .unwrap_or(original_ty);
+        quote! { 
+            pub fn #name(&mut self, #name: #set_ty) -> &mut Self {
+                self.#name = Some(#name);
+                self
             }
         }
     });
 
     let builder_set_each_method = fields.iter().map(|f| {
-        if !&f.attrs.is_empty() {
-            if f.attrs.len() != 1 {
-                return make_err(&f.ident, EXPECT_EACH_ATTR_TEMPLATE).into();
-            }
-
-            let each_ident_result = get_each_attr(&f.attrs, &f.ident.clone().unwrap());
-
-            let each_ident = match each_ident_result {
-                Ok(i) => i,
-                Err(e) => return e.into(),
-            };
-
-            let name = &f.ident;
-            let original_ty = &f.ty;
-            let inner_ty = inner_type("Vec", original_ty);
-            if inner_ty.is_none() {
-                return make_err(f, r#"Fields must have Vec type to use the "each"" attribute"#).into();
-            }
-            let set_ty = inner_ty.unwrap();
-
-            if is_type("String", set_ty) {
-                Some(quote! { 
-                    pub fn #each_ident<T: Into<String>>(&mut self, #each_ident: T) -> &mut Self {
-                        let arg = #each_ident.into();
-                        if self.#name.is_none() {
-                            self.#name = Some(vec![]);
-                        }
-                        if let Some(ref mut vector) = self.#name {
-                            vector.push(arg);
-                        } else {
-                            unreachable!();
-                        }
-                        self
-                    }
-                })
-            } else {
-                Some(quote! { 
-                    pub fn #each_ident(&mut self, #each_ident: #set_ty) -> &mut Self {
-                        if self.#name.is_none() {
-                            self.#name = vec![];
-                        }
-                        if let Some(ref mut vector) = self.#name {
-                            vector.push(#each_ident);
-                        } else {
-                            unreachable!();
-                        }
-                        self
-                    }
-                })
-            }
-        } else {
-            None
+        if f.attrs.is_empty() {
+            return None;
         }
+
+        if f.attrs.len() != 1 {
+            return utils::make_err(&f.ident, utils::EXPECT_EACH_ATTR_TEMPLATE).into();
+        }
+
+        let each_ident_result = if let Some(field_ident) = &f.ident {
+            utils::get_each_attr(&f.attrs, field_ident)
+        } else {
+            return utils::make_err(&f.ident, "Expect field ident").into();
+        };
+
+        let each_ident = match each_ident_result {
+            Ok(i) => i,
+            Err(e) => return e.into(),
+        };
+
+        let name = &f.ident;
+        let original_ty = &f.ty;
+
+        // Custom set each method for Vec<String> or Option<Vec<String>>
+        // These method accept T where T:Into<String> as argument
+        if utils::is_type_vec_string(original_ty) || 
+            utils::is_type_option_vec_string(original_ty) {
+            return Some(quote! { 
+                pub fn #each_ident<T: Into<String>>(&mut self, #each_ident: T) -> &mut Self {
+                    let arg = #each_ident.into();
+                    if self.#name.is_none() {
+                        self.#name = Some(vec![]);
+                    }
+                    if let Some(ref mut vector) = self.#name {
+                        vector.push(arg);
+                    } else {
+                        unreachable!();
+                    }
+                    self
+                }
+            });
+        }
+
+        let set_ty = if let Some(inner_ty) = utils::inner_type("Vec", original_ty) {
+            inner_ty
+        } else {
+            return utils::make_err(f, r#"Fields must have Vec type to use the "each" attribute"#).into();
+        };
+
+        Some(quote! { 
+            pub fn #each_ident(&mut self, #each_ident: #set_ty) -> &mut Self {
+                if self.#name.is_none() {
+                    self.#name = vec![];
+                }
+                if let Some(ref mut vector) = self.#name {
+                    vector.push(#each_ident);
+                } else {
+                    unreachable!();
+                }
+                self
+            }
+        })
     });
 
     let builder_check_build_field = fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-        if inner_type("Option", ty).is_some() {
+        if utils::is_type("Option", ty) {
             quote! { 
                 #name: self.#name.clone(),
             }
@@ -268,128 +295,3 @@ pub fn instruction_builder(input: TokenStream) -> TokenStream {
     }.into()
 }
 
-fn inner_type<'a>(wrapper_ty: &str, ty: &'a syn::Type) -> std::option::Option<&'a syn::Type> {
-    if let syn::Type::Path(ref p) = ty {
-       if p.path.segments.len() != 1 || p.path.segments[0].ident != wrapper_ty {
-           return std::option::Option::None;
-       }
-
-       if let syn::PathArguments::AngleBracketed(ref inner_ty_args) = p.path.segments[0].arguments {
-           if inner_ty_args.args.len() != 1 {
-               return std::option::Option::None;
-           } 
-
-           let arg = inner_ty_args.args.first().unwrap();
-           if let syn::GenericArgument::Type(ref inner_ty) = arg {
-               return std::option::Option::Some(inner_ty);
-           }
-       }
-    }
-    std::option::Option::None
-}
-
-fn is_type(ty_name: &str, ty: &syn::Type) -> bool {
-    if let syn::Type::Path(ref p) = ty {
-       if p.path.segments.len() == 1 && p.path.segments[0].ident == ty_name {
-           return true;
-       }
-    }
-    false
-}
-
-fn make_err<T: quote::ToTokens>(t: T, msg: &str) -> proc_macro2::TokenStream {
-    syn::Error::new_spanned(t, msg).to_compile_error()
-}
-
-struct AttrData {
-    instruction_name: syn::Ident,
-    value_method: syn::Ident,
-}
-
-const EXPECT_ATTR_TEMPLATE: &str = r#"Expected 
-#[instruction_builder(
-    instruction_name = <name>, 
-    value_method = <method>,
-)]"#;
-
-const EXPECT_EACH_ATTR_TEMPLATE: &str = r#"Expected 
-#[instruction_builder(each = <arg>)]"#;
-
-fn get_each_attr(attr: &Vec<syn::Attribute>, struct_ident: &syn::Ident) -> Result<syn::Ident, proc_macro2::TokenStream> {
-    if attr.len() != 1 {
-        return Err(make_err(struct_ident, EXPECT_EACH_ATTR_TEMPLATE));
-    }
-    if let syn::Meta::List( ref metalist ) = &attr[attr.len() - 1].meta {
-        let tokenstream = &mut metalist.tokens.clone().into_iter();
-
-        verify_attr_ident(tokenstream.next(), "each", metalist, EXPECT_EACH_ATTR_TEMPLATE)?;
-        verify_attr_punct(tokenstream.next(), '=', metalist, EXPECT_EACH_ATTR_TEMPLATE)?;
-
-        let each_ident = match tokenstream.next() {
-            Some(TokenTree::Ident(ref i)) => i.clone(),
-            _ => return Err(make_err(metalist, EXPECT_EACH_ATTR_TEMPLATE)),
-        };
-
-        Ok(each_ident)
-    } else {
-        return Err(make_err(struct_ident, EXPECT_EACH_ATTR_TEMPLATE));
-    }
-}
-
-fn get_attr(attr: &Vec<syn::Attribute>, struct_ident: &syn::Ident) -> Result<AttrData, proc_macro2::TokenStream> {
-    if attr.is_empty() {
-        return Err(make_err(struct_ident, EXPECT_ATTR_TEMPLATE));
-    }
-
-    // first attr can be doc
-    if let syn::Meta::List( ref metalist ) = &attr[attr.len() - 1].meta {
-        let tokenstream = &mut metalist.tokens.clone().into_iter();
-
-        verify_attr_ident(tokenstream.next(), "instruction_name", metalist, EXPECT_ATTR_TEMPLATE)?;
-        verify_attr_punct(tokenstream.next(), '=', metalist, EXPECT_ATTR_TEMPLATE)?;
-
-        let instruction_name = match tokenstream.next() {
-            Some(TokenTree::Ident(ref i)) => i.clone(),
-            _ => return Err(make_err(metalist, EXPECT_ATTR_TEMPLATE)),
-        };
-
-        verify_attr_punct(tokenstream.next(), ',', metalist, EXPECT_ATTR_TEMPLATE)?;
-        verify_attr_ident(tokenstream.next(), "value_method", metalist, EXPECT_ATTR_TEMPLATE)?;
-        verify_attr_punct(tokenstream.next(), '=', metalist, EXPECT_ATTR_TEMPLATE)?;
-
-        let value_method = match tokenstream.next() {
-            Some(TokenTree::Ident(ref i)) => i.clone(),
-            _ => return Err(make_err(metalist, EXPECT_ATTR_TEMPLATE)),
-        };
-
-        Ok(AttrData { instruction_name, value_method })
-    } else {
-        return Err(make_err(struct_ident, EXPECT_ATTR_TEMPLATE));
-    }
-}
-
-fn verify_attr_ident<T: quote::ToTokens>(token: Option<TokenTree>, expected_ident: &str, span: T, err_msg: &str) -> Result<(), proc_macro2::TokenStream> {
-    match token {
-        Some(TokenTree::Ident(ref i)) => {
-            if i != expected_ident {
-                return Err(make_err(span, err_msg));
-            }
-        },
-        _ => return Err(make_err(span, err_msg)),
-
-    }
-    Ok(())
-}
-
-fn verify_attr_punct<T: quote::ToTokens>(token: Option<TokenTree>, expected_punct: char, span: T, err_msg: &str) -> Result<(), proc_macro2::TokenStream> {
-    match token {
-        Some(TokenTree::Punct(ref p)) => {
-            if p.as_char() != expected_punct {
-                return Err(make_err(span, err_msg));
-            }
-        },
-        _ => return Err(make_err(span, err_msg)),
-
-    }
-    Ok(())
-}
---
+++ dockerfile_derive/src/utils.rs
@@ -0,0 +1,151 @@
+use proc_macro2::TokenTree;
+
+pub(crate) fn inner_type<'a>(wrapper_ty: &str, ty: &'a syn::Type) -> std::option::Option<&'a syn::Type> {
+    if let syn::Type::Path(ref p) = ty {
+       if p.path.segments.len() != 1 || p.path.segments[0].ident != wrapper_ty {
+           return std::option::Option::None;
+       }
+
+       if let syn::PathArguments::AngleBracketed(ref inner_ty_args) = p.path.segments[0].arguments {
+           if inner_ty_args.args.len() != 1 {
+               return std::option::Option::None;
+           } 
+
+           let arg = inner_ty_args.args.first().unwrap();
+           if let syn::GenericArgument::Type(ref inner_ty) = arg {
+               return std::option::Option::Some(inner_ty);
+           }
+       }
+    }
+    std::option::Option::None
+}
+
+pub(crate) fn is_type(ty_name: &str, ty: &syn::Type) -> bool {
+    if let syn::Type::Path(ref p) = ty {
+       if p.path.segments.len() == 1 && p.path.segments[0].ident == ty_name {
+           return true;
+       }
+    }
+    false
+}
+
+pub(crate) fn is_type_vec_string(ty: &syn::Type) -> bool {
+    if let Some(inner_of_vec) = inner_type("Vec", ty) {
+        return is_type("String", inner_of_vec);
+    }
+    false
+}
+
+pub(crate) fn is_type_option_vec_string(ty: &syn::Type) -> bool {
+    if let Some(inner_of_option) = inner_type("Option", ty) {
+        if let Some(inner_of_option_vec) = inner_type("Vec", inner_of_option) {
+            return is_type("String", inner_of_option_vec);
+        }
+    }
+    false
+}
+
+pub(crate) fn is_type_option_string(ty: &syn::Type) -> bool {
+    if let Some(inner_of_option) = inner_type("Option", ty) {
+        return is_type("String", inner_of_option);
+    }
+    false
+}
+
+pub(crate) fn make_err<T: quote::ToTokens>(t: T, msg: &str) -> proc_macro2::TokenStream {
+    syn::Error::new_spanned(t, msg).to_compile_error()
+}
+
+pub(crate) struct AttrData {
+    pub(crate) instruction_name: syn::Ident,
+    pub(crate) value_method: syn::Ident,
+}
+
+const EXPECT_ATTR_TEMPLATE: &str = r#"Expected 
+#[instruction_builder(
+    instruction_name = <name>, 
+    value_method = <method>,
+)]"#;
+
+pub(crate) const EXPECT_EACH_ATTR_TEMPLATE: &str = r#"Expected 
+#[instruction_builder(each = <arg>)]"#;
+
+pub(crate) fn get_each_attr(attr: &Vec<syn::Attribute>, struct_ident: &syn::Ident) -> Result<syn::Ident, proc_macro2::TokenStream> {
+    if attr.len() != 1 {
+        return Err(make_err(struct_ident, EXPECT_EACH_ATTR_TEMPLATE));
+    }
+    if let syn::Meta::List( ref metalist ) = &attr[attr.len() - 1].meta {
+        let tokenstream = &mut metalist.tokens.clone().into_iter();
+
+        verify_attr_ident(tokenstream.next(), "each", metalist, EXPECT_EACH_ATTR_TEMPLATE)?;
+        verify_attr_punct(tokenstream.next(), '=', metalist, EXPECT_EACH_ATTR_TEMPLATE)?;
+
+        let each_ident = match tokenstream.next() {
+            Some(TokenTree::Ident(ref i)) => i.clone(),
+            _ => return Err(make_err(metalist, EXPECT_EACH_ATTR_TEMPLATE)),
+        };
+
+        Ok(each_ident)
+    } else {
+        return Err(make_err(struct_ident, EXPECT_EACH_ATTR_TEMPLATE));
+    }
+}
+
+pub(crate) fn get_attr(attr: &Vec<syn::Attribute>, struct_ident: &syn::Ident) -> Result<AttrData, proc_macro2::TokenStream> {
+    if attr.is_empty() {
+        return Err(make_err(struct_ident, EXPECT_ATTR_TEMPLATE));
+    }
+
+    // first attr can be doc
+    if let syn::Meta::List( ref metalist ) = &attr[attr.len() - 1].meta {
+        let tokenstream = &mut metalist.tokens.clone().into_iter();
+
+        verify_attr_ident(tokenstream.next(), "instruction_name", metalist, EXPECT_ATTR_TEMPLATE)?;
+        verify_attr_punct(tokenstream.next(), '=', metalist, EXPECT_ATTR_TEMPLATE)?;
+
+        let instruction_name = match tokenstream.next() {
+            Some(TokenTree::Ident(ref i)) => i.clone(),
+            _ => return Err(make_err(metalist, EXPECT_ATTR_TEMPLATE)),
+        };
+
+        verify_attr_punct(tokenstream.next(), ',', metalist, EXPECT_ATTR_TEMPLATE)?;
+        verify_attr_ident(tokenstream.next(), "value_method", metalist, EXPECT_ATTR_TEMPLATE)?;
+        verify_attr_punct(tokenstream.next(), '=', metalist, EXPECT_ATTR_TEMPLATE)?;
+
+        let value_method = match tokenstream.next() {
+            Some(TokenTree::Ident(ref i)) => i.clone(),
+            _ => return Err(make_err(metalist, EXPECT_ATTR_TEMPLATE)),
+        };
+
+        Ok(AttrData { instruction_name, value_method })
+    } else {
+        return Err(make_err(struct_ident, EXPECT_ATTR_TEMPLATE));
+    }
+}
+
+pub(crate) fn verify_attr_ident<T: quote::ToTokens>(token: Option<TokenTree>, expected_ident: &str, span: T, err_msg: &str) -> Result<(), proc_macro2::TokenStream> {
+    match token {
+        Some(TokenTree::Ident(ref i)) => {
+            if i != expected_ident {
+                return Err(make_err(span, err_msg));
+            }
+        },
+        _ => return Err(make_err(span, err_msg)),
+
+    }
+    Ok(())
+}
+
+pub(crate) fn verify_attr_punct<T: quote::ToTokens>(token: Option<TokenTree>, expected_punct: char, span: T, err_msg: &str) -> Result<(), proc_macro2::TokenStream> {
+    match token {
+        Some(TokenTree::Punct(ref p)) => {
+            if p.as_char() != expected_punct {
+                return Err(make_err(span, err_msg));
+            }
+        },
+        _ => return Err(make_err(span, err_msg)),
+
+    }
+    Ok(())
+}
+
