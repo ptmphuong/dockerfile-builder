250e74285e102f51f7126bf157bfa252b0ae85db
Phuong Pham <ptmphuong9@gmail.com>
2023-07-12 15:47:21 -0700

    add doc

--- dockerfile_builder/src/instruction.rs
+++ dockerfile_builder/src/instruction.rs
@@ -1,5 +1,31 @@
+//! Dockerfile Instruction definitions
+//!
+//! See [`Instruction`]
+
 use dockerfile_derive::InstructionInit;
 
+/// Dockerfile Instructions
+///
+/// There are 2 ways to build an Instruction:
+/// * from string literals, or 
+/// * using the [`instruction_builder`] interface.
+///
+/// [`instruction_builder`]: crate::instruction_builder
+///
+/// ```
+/// use dockerfile_builder::instruction::FROM;
+/// use dockerfile_builder::instruction_builder::FromBuilder;
+///
+/// let from = FROM::from("cargo-chef AS chef");
+///
+/// let from_by_builder = FromBuilder::builder()
+///     .image("cargo-chef")
+///     .name("chef")
+///     .build()
+///     .unwrap();
+/// 
+/// assert_eq!(from, from_by_builder);
+/// ```
 //#[derive(Debug, Clone, Eq, PartialEq)]
 #[derive(Debug, InstructionInit, Clone, Eq, PartialEq)]
 pub enum Instruction {
